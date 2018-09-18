796006441a50a6805d85e2e16d818a802d856215
redeyes361 <redeyes_361@hotmail.com>
2018-09-18 14:43:39 +0200

    first commit

---
+++ TAAProject/src/main/java/istic/m2/taa/project/TAAProject/TaaProjectApplication.java
@@ -0,0 +1,13 @@
+package istic.m2.taa.project.TAAProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaaProjectApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TaaProjectApplication.class, args);
+	}
+}
---
+++ TAAProject/src/test/java/istic/m2/taa/project/TAAProject/TaaProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package istic.m2.taa.project.TAAProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaaProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
