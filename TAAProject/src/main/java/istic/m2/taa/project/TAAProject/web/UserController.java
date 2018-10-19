1c0f23a3fde651c0392df8e6c7ee2e8837610fa3
redeyes361 <redeyes_361@hotmail.com>
2018-10-19 14:37:52 +0200

    change cors politics

--- TAAProject/src/main/java/istic/m2/taa/project/TAAProject/web/UserController.java
+++ TAAProject/src/main/java/istic/m2/taa/project/TAAProject/web/UserController.java
@@ -1,5 +1,6 @@
 package istic.m2.taa.project.TAAProject.web;
 
+import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 import javax.ws.rs.core.MediaType;
@@ -9,6 +10,8 @@ import javax.ws.rs.core.Response.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,10 +60,14 @@ public class UserController {
 	 return userDAO.findByName(pseudo);
 	}
 	
+	//@CrossOrigin(origins="http://localhost:4200")
 	@PostMapping(value="/user",produces={MediaType.APPLICATION_JSON,MediaType.TEXT_PLAIN},consumes=MediaType.APPLICATION_JSON)
 	public Response addUser(@RequestBody User user){
+		System.out.println("coucou");
+		String password = Base64.getDecoder().decode(user.getPassword()).toString();
+		user.setPassword( new BCryptPasswordEncoder().encode(password));
 		userDAO.save(user);
-		return Response.noContent().status(Status.ACCEPTED)	
+		return Response.ok(user.getId()).status(Status.ACCEPTED)	
 					   .build();
 	}
 	
